54ba7e002c2ced56b0d1475ee2b31ce5d1000ff5
Mankindi-Ganesh <ganeshmankindi@gmail.com>
2025-05-15 19:35:04 +0530

    appointment page.

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -5,13 +5,13 @@ const Contact = () => {
   return (
    <div>
     
-      <div className="text-center text-2xl pt-10 pb-10 text-gray-500">
+      <div className="text-center text-2xl pt-10 text-gray-500">
         <p>CONTACT  <span className="text-gray-700 font-semibold">US</span></p>
       </div>
     <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm">
     <img className=" w-full md:max-w-[360px]" src={assets.contact_image} alt=""/>
     <div className="flex flex-col justify-center items-start gap-6">
-      <p className="text-gray-600 text-lg pt-10 font-semibold">OUR OFFICE</p>
+      <p className="text-gray-600 text-lg pt-0 font-semibold">OUR OFFICE</p>
       <p className="text-gray-500">00000 Willms Station
       Suite 000, Washington, USA</p>
       <p className="text-gray-500">Tel: (000) 000-0000
