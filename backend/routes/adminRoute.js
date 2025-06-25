ea19c98426c0f92b6cf8140aa8cba5e5dabfe7fc
Mankindi-Ganesh <ganeshmankindi@gmail.com>
2025-06-25 09:55:38 +0530

    doctors added in admin panel

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
 import upload from "../utils/multer.js";  // Correct path here
-import { addDoctor,loginAdmin} from "../controllers/adminController.js";
+import { addDoctor,allDoctors,loginAdmin} from "../controllers/adminController.js";
 import authAdmin from '../middleware/authAdmin.js';
 const adminRouter = express.Router();
 
 adminRouter.post("/add-doctor", authAdmin,upload.single("image"), addDoctor);
 adminRouter.post("/login",loginAdmin);
+adminRouter.post("/all-doctors",authAdmin,allDoctors);
+
+
 export default adminRouter;
